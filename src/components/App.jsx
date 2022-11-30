e3b86d693178bc2533f6bd9586cdb34d3e48cf84
Jefferson-Benavides <benavidesjefferson18@gmail.com>
2022-11-29 21:59:34 -0500

    Componente Login.jsx y estilos se ejecutan correctamente

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Layout from "../containers/Layout";
+import Login from "../containers/Login";
+import '../styles/global.css';
 
 const App = () => {
     return (
-        <h1>Hola Jeffer</h1>
+        <Layout>
+            <Login />
+            {/* <Login></Login> */}
+        </Layout>
     );
 }
 
